4d485fdb8c800b0b09cacefaa04ec5fd518dae18
neeleyc <33631679+neeleyc@users.noreply.github.com>
2018-05-14 11:15:13 -0400

    Add files via upload

---
+++ siri NC.py
@@ -0,0 +1,25 @@
+import win32com.client as wincl
+import speech_recognition as sr
+import webbrowser as wb
+
+speak = wincl.Dispatch("SAPI.SpVoice")
+
+r = sr. Recognizer()
+with sr. Microphone() as source:
+    speak.Speak("Hi Comp Sci class, what video should we watch?")
+    print("Listening...")
+    audio = r.listen(source)
+    print("Thinking...")
+
+
+try:
+    words = r.recognize_google(audio)
+    speak.Speak("Ok Neeley, let's look for " + r.recognize_google(audio))
+    wb.open("https://www.youtube.com/results?search_query=" + words)
+
+except sr. UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service;{0}".format(e))
+
+    
